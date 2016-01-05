68c80a51fe13d24cb02eb57880042f83f9c3a3e6
crolabear <crolabear@gmail.com>
2016-01-04 23:40:17 -0800

    added another method that finds uniq and dup using dictionary

--- findUnique.py
+++ findUnique.py
@@ -22,11 +22,31 @@ def findUniq(input1):
     return uniq
 
 
+
+def method2(input1):
+# use index. put everything in a dictionary as letters, and counts
+    newDict = {}
+    kz = ''
+    for item in input1:
+        if item not in newDict.keys():
+            newDict[item] = 1
+        else:
+            newDict[item] = newDict[item] + 1
+# now loop through the dictionary to print out the number of occurrance 1
+    for kz,vz in newDict.items():
+        if vz == 1:   # if we want to find dups, we will use vz > 1
+            uniq = kz
+
+    return uniq
+
+
 def main():
     input1 = [1,1,2,2,3,3,4]
     k = findUniq(input1)
     print k
 
+    k = method2(input1)
+    print k 
 
 
 if __name__ == "__main__":
